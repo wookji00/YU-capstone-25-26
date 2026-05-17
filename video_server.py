27d391e6ba2f10d7666af4513a6e1760e4af9c7f
synartisi <232056008+synartisi@users.noreply.github.com>
2026-05-17 23:35:28 +0900

    카메라 스트리밍 코드 정리

--- video_server.py
+++ video_server.py
@@ -1,11 +1,14 @@
 import cv2
 import time
 import threading
+import json
+from datetime import datetime
 from flask import Flask, Response, render_template
 from picamera2 import Picamera2
 from libcamera import Transform
 from PIL import ImageFile
 
+fps_value = 0.0
 # 데이터가 일부 잘려도 에러를 내지 않고 로드하도록 설정
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -14,7 +17,7 @@ app = Flask(__name__)
 # 1 Picam2를 전역으로 한번 초기화
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration(
-    main={"size": (640, 480), "format": "MJPEG"}, transform=Transform(hflip=False, vflip=False)
+    main={"size": (640, 480), "format": "BGR888"}, transform=Transform(hflip=False, vflip=False)
 )
 picam2.configure(video_config)
 picam2.start()
@@ -26,13 +29,32 @@ frame_lock = threading.Lock()
 
 def capture_frames():
     """백그라운드 스레드에서 지속적으로 프레임을 캡처하여 global_frame 갱신"""
-    global global_frame
+    global global_frame, fps_value
+    frame_count = 0
+    last_time = time.time()
     while True:
         # picam2 로부터 프레임 캡처 (numpy array)
         frame_data = picam2.capture_array()
 
         # 필요하면 OpenCV 처리
         frame_data = cv2.cvtColor(frame_data, cv2.COLOR_RGB2BGR)
+        
+        # FPS count
+        frame_count += 1
+        current_time = time.time()
+        if current_time - last_time >= 1.0:
+            fps_value = frame_count / (current_time - last_time)
+            frame_count = 0
+            last_time = current_time
+            
+            # ? ?? ?? ?? ??
+            # 'fps_log.txt' ??? ??? ?? FPS? ???? ?????.
+            try:
+                with open("fps_log.txt", "a") as f:
+                    now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    f.write(f"[{now_str}] FPS: {fps_value:.2f}\n")
+            except Exception as e:
+                print(f"?? ?? ??: {e}")
 
         # 프레임 을 JPEG 로 인코딩
         ret, buffer = cv2.imencode(".jpg", frame_data)
@@ -76,6 +98,10 @@ def video_feed():
     # 여러 클라이언트가 접근해도 동일한 global_frame을 읽어 스트리밍
     return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
+# fps 요청
+@app.route("/get_fps")
+def get_fps(): # 현재 fps rate를 요청 할때 보내줌
+    return json.dumps({"fps": round(fps_value, 2)})
 
 # 서버가 모든 네트워크 인터페이스에서 접근 가능하게 하기
 if __name__ == "__main__":
@@ -86,5 +112,7 @@ if __name__ == "__main__":
         # 이 기능이 초기화 코드를 2번 실행하게 할수도 있으므로 비활성화
     finally:
         picam2.stop()
+
+
 
 # mkdir -p template 후 index.html 만들기
